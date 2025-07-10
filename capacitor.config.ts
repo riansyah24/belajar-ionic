5f80370350d78dbb01dc8515197ffeccc91c7e72
teukuriansyah <teukuriansyahkarim@gmail.com>
2025-07-11 00:50:47 +0700

    belajar

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.belajarionic.app',
+  appName: 'Belajar',
+  webDir: 'build'
+};
+
+export default config;
