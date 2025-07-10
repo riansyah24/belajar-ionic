333300cef4474c397564b9d82856c942870d439b
riansyah24 <125286563+riansyah24@users.noreply.github.com>
2025-07-10 18:09:19 +0000

    fix:fix splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.belajarionic.app',
   appName: 'Belajar',
-  webDir: 'build'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      backgroundColor: "#000000",
+      androidScaleType: "CENTER_CROP",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
